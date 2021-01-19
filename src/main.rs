0e6f018e75a07ff2a63ce4bc9993c7602421ae4b
Connor Monks <monkslc@gmail.com>
2021-01-18 20:11:51 -0500

    accept expression as command line argument instead of stdin

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
-use std::io::{self, Read};
+use std::env;
 
 use rust_calc::eval_expression;
 
 fn main() {
-    let mut expression = String::new();
-    let mut stdin = io::stdin();
-    stdin
-        .read_to_string(&mut expression)
-        .expect("Failed to get expression");
-
-    eval_expression(&expression);
+    let expression: String = env::args().skip(1).collect();
+    println!("{}", eval_expression(&expression));
 }
